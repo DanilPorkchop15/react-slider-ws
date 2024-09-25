e2e8a2fb369b4bdc9d13d4d427b6cd869ac549af
danilPorkchop15 <danilkostin150705@gmail.com>
2024-09-25 16:59:30 +0300

    feat(slider): modified slider styles

---
+++ src/components/Slider/SliderStyled.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const SliderContainerStyled = styled.div`
+  width: 100%;
+  overflow: hidden;
+`;
+
+export const SliderStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+justify-content: center;
+  transition: transform 0.3s ease-in-out;
+`;
+
+export const SlideContainerStyled = styled.div`
+  flex: 0 0 100%;
+  height: 100%;
+  width: 100%;
+`;
+
+export const SlideStyled = styled.img`
+  height: 400px
+`;
+
