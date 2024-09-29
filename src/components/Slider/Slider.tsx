05d96098938cffb12afaa4fd8910ec8934c07ebe
danilPorkchop15 <danilkostin150705@gmail.com>
2024-09-29 21:01:12 +0300

    feat(slider): add animation

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,8 +1,16 @@
 import { FC, useState, useEffect, useCallback, useRef } from "react";
-import { SliderContainerStyled, SliderStyled, SlideContainerStyled, SlideStyled, SliderTitleStyled, SliderPagesStyled } from "./SliderStyled";
+import {
+  SliderContainerStyled,
+  SliderStyled,
+  SlideContainerStyled,
+  SlideStyled,
+  SliderTitleStyled,
+  SliderPagesStyled,
+} from "./SliderStyled";
 import { Slide } from "../../types/Slider.types";
 import SliderNav from "./SliderNav/SlideNav";
 import SliderPagination from "./SliderPagination/SliderPagination";
+import { AnimatePresence, motion } from "framer-motion";
 
 interface SliderProps {
   slides: Slide[];
@@ -25,11 +33,13 @@ const Slider: FC<SliderProps> = ({
 }) => {
   const [currentSlide, setCurrentSlide] = useState<number>(0);
   const autoInterval = useRef<NodeJS.Timer | null>(null);
+  const [direction, setDirection] = useState<"next" | "prev">("next");
 
   const handleInterval = useCallback(() => {
     const nextSlide: (prevSlide: number) => number = loop
       ? (prevSlide) => (prevSlide + 1) % slides.length
-      : (prevSlide) => prevSlide === slides.length - 1 ? prevSlide : prevSlide + 1;
+      : (prevSlide) =>
+          prevSlide === slides.length - 1 ? prevSlide : prevSlide + 1;
     setCurrentSlide(nextSlide);
   }, [slides.length, loop]);
 
@@ -50,18 +60,24 @@ const Slider: FC<SliderProps> = ({
     autoInterval.current = setInterval(handleInterval, delay * 1000);
   };
 
-  const handleNavClick = useCallback((isNext: boolean) => {
+  const handleNavClick = useCallback(
+    (isNext: boolean) => {
+      setDirection(isNext ? "next" : "prev");
 
-    if (isNext) {
-      setCurrentSlide((currentSlide + 1) % slides.length);
-    } else {
-      setCurrentSlide((currentSlide - 1) < 0 ? slides.length - 1 : currentSlide - 1);
-    }
-  }, [currentSlide, setCurrentSlide, slides.length]);
+      if (isNext) {
+        setCurrentSlide((currentSlide + 1) % slides.length);
+      } else {
+        setCurrentSlide(
+          currentSlide - 1 < 0 ? slides.length - 1 : currentSlide - 1,
+        );
+      }
+    },
+    [currentSlide, slides.length],
+  );
 
   const handleSelectPage = (page: number) => {
     setCurrentSlide(page);
-  }
+  };
 
   return (
     <SliderContainerStyled>
@@ -70,25 +86,36 @@ const Slider: FC<SliderProps> = ({
         onMouseLeave={() => auto && stopMouseHover && handleMouseLeave()}
       >
         <SlideContainerStyled>
-          <SlideStyled
-            src={slides[currentSlide].img}
-            alt={slides[currentSlide].text}
-          />
-          {navs && <SliderNav
-            isNext={currentSlide !== slides.length - 1 || loop}
-            isPrevious={currentSlide !== 0 || loop}
-            onNavClick={handleNavClick}
-          />}
+          <AnimatePresence mode="wait">
+            <SlideStyled
+              as={motion.img}
+              key={currentSlide}
+              src={slides[currentSlide].img}
+              alt={slides[currentSlide].text}
+              initial={{ opacity: 0, x: direction === "next" ? 100 : -100 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: direction === "next" ? -100 : 100 }}
+              transition={{ duration: 0.5 }}
+            />
+          </AnimatePresence>
+          {navs && (
+            <SliderNav
+              isNext={currentSlide !== slides.length - 1 || loop}
+              isPrevious={currentSlide !== 0 || loop}
+              onNavClick={handleNavClick}
+            />
+          )}
         </SlideContainerStyled>
         <SliderTitleStyled>{slides[currentSlide].text}</SliderTitleStyled>
         <SliderPagesStyled>{`${currentSlide + 1} / ${slides.length}`}</SliderPagesStyled>
       </SliderStyled>
-      {pages && <SliderPagination
-        onSelectPage={handleSelectPage}
-        size={slides.length}
-        current={currentSlide}
-      />}
-
+      {pages && (
+        <SliderPagination
+          onSelectPage={handleSelectPage}
+          size={slides.length}
+          current={currentSlide}
+        />
+      )}
     </SliderContainerStyled>
   );
 };
